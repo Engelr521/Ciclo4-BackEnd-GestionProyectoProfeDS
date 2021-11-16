e4333eb7bf126ccc345ce915b5ed8b193c88f3b8
Leonardo Ramirez <angelr521@hotmail.com>
2021-11-15 19:22:56 -0500

    Explicacion de creacion de tabla de proyectos en MongoDb con la opcion de buscar y crear proyectos

--- index.ts
+++ index.ts
@@ -1,18 +1,50 @@
 import conectarBD from "./db/db";
 import { UserModel } from "./models/user";
 import { Enum_Rol } from "./models/enums";
-import { getParsedCommandLineOfConfigFile } from "typescript";
+import { ProjectModel } from "./models/project";
 
 const main = async () =>{
     await conectarBD();
 
-    //Crear usuario
+    //Crear Un proyecto en MongoDB 
+    // ProjectModel.create({
+    //     nombre: 'Proyecto 3',
+    //     presupuesto: 520,
+    //     fechaInicio: Date.now(),
+    //     fechaFin: new Date("2024/11/10"),
+    //     lider: '6192c3b91e466ef676e52236',
+    // })
+    //     .then((p) => {
+    //         console.log('Proyecto Creado',p);
+    //     })
+    //     .catch((e) => {
+    //         console.error('Error en la creacion del proyecto',e)
+    //     });
+
+    //Consultar Proyectos de la coleccion proyectos, junto con su lider de la coleccion de ususarios
+    // const proyecto = await ProjectModel.find({nombre: 'Proyecto 3'}).populate('lider')
+    // .then((proyecto) => {
+    //         console.log('El proyecto es: ', proyecto);
+    //     })
+    //     .catch((e) => {
+    //         console.error('Error al buscar el proyecto', e)
+    //     });
+
+    
+
+};
+
+main();
+
+// CRUD USUARIOS
+
+ //Crear usuario
     // UserModel.create({
     //     correo: 'ramirezleo52@gmail.com',
     //     identificacion: '324655929',
     //     nombre: 'leonardo',
     //     apellido: 'Ramirez',
-    //     rol: Enum_Rol.estudiante,
+    //     rol: Enum_Rol.lider,
     // })
     //     .then((u) => {
     //         console.log('usuario Creado', u);
@@ -60,9 +92,3 @@ const main = async () =>{
     //     .catch(e=>{
     //         console.error('Error al elminar un usuario',e);
     // });
-
-    
-
-};
-
-main();
